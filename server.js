7bd54bbb064bbf02423bbd0d48813f61872b6b08
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-03-13 16:49:44 -0500

    added functions to createNewAnimal and validateAnimal as proper responses for GET and POST requests; implemented Insomnia Core

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 
 const express = require('express');
@@ -5,6 +8,11 @@ const express = require('express');
 //* Sets default port *//
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 //* Function to filter by query *//
 
@@ -53,6 +61,34 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  //* Function that takes req.body and writes data to animals.json *//
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
+
+  //* Function to validate json data from req.body *//
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
   //* GET route callback to get JSON data from Express server *//
   app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -72,13 +108,21 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
-  //* User populate animals method */
+  //* User populate animals method(post route callback)  */
   app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
   });
 
+  
   //* backend port app is listening on *//
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
